2bd857e8d5f185ec73e4eda7e845f2d7004411fa
MeenasuryaAutomationTester <meenasurya@gmail.com>
2025-07-15 16:15:29 +0530

    eclipse to git

---
+++ src/loginmodule/Login.java
@@ -0,0 +1,12 @@
+package loginmodule;
+
+public class Login {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("shashikala");
+		System.out.println("jaya");
+
+	}
+
+}
