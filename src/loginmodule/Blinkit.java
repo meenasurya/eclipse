ed3fd4c4d9d55173005e5790126136cba4c612e6
MeenasuryaAutomationTester <meenasurya@gmail.com>
2025-07-16 12:08:05 +0530

    hello

---
+++ src/loginmodule/Blinkit.java
@@ -0,0 +1,12 @@
+package loginmodule;
+
+public class Blinkit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("dinesh");
+		System.out.println("venky");
+
+	}
+
+}
